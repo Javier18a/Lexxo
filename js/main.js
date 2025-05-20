09032fbddc166e8a9de1f6d32e21a8bc66ce1708
Javier <161880469+Javier18a@users.noreply.github.com>
2025-05-19 19:10:00 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function showProductInfo(producto) {
+    document.getElementById('modal-img').src = producto.imagen;
+    document.getElementById('modal-nombre').textContent = producto.nombre;
+    document.getElementById('modal-precio').textContent = 'Precio: ' + producto.precio;
+    document.getElementById('modal-material').textContent = 'Material: ' + producto.material;
+    if (producto.oferta) {
+        document.getElementById('modal-oferta').textContent = 'Oferta: ' + producto.oferta;
+        document.getElementById('modal-oferta').style.display = 'block';
+    } else {
+        document.getElementById('modal-oferta').style.display = 'none';
+    }
+    document.getElementById('product-modal').style.display = 'flex';
+}
+function closeProductInfo() {
+    document.getElementById('product-modal').style.display = 'none';
+}
+
+ const sliderImages = [
+          "img/product - LX/Logo-black-removebg-preview.png",
+          "img/product - LX/C1-removebg-preview.png",
+          "img/product - LX/S2-removebg-preview.png",
+          "img/product - LX/C4-removebg-preview.png",
+          "img/product - LX/S5-removebg-preview.png"
+        ];
+        let sliderIndex = 0;
+        const sliderImg = document.getElementById('slider-img');
+
+        function showNextImage() {
+          sliderImg.style.opacity = 0;
+          setTimeout(() => {
+            sliderIndex = (sliderIndex + 1) % sliderImages.length;
+            sliderImg.src = sliderImages[sliderIndex];
+            sliderImg.onload = () => {
+              sliderImg.style.opacity = 1;
+            };
+          }, 500);
+        }
+        setInterval(showNextImage, 2200);
